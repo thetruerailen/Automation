df307106e821d8baef40d0db8b8e64072502846c
Keet DeMoss <103075692+thetruerailen@users.noreply.github.com>
2023-03-16 12:00:00 -0500

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,14 @@ import sys
 import subprocess
 import time
 
+import json
+import sys
+import subprocess
+import time
+import math
+import random
+import os
+
 def run_command(command):
     """
     Executes the specified command and returns the output.
@@ -32,6 +40,128 @@ def end_command():
     Signals the end of the script.
     """
     sys.exit(0)
+
+def input_command(prompt):
+    """
+    Prompts the user for input and returns the input.
+    """
+    return input(prompt)
+
+def if_command(condition, true_commands, false_commands):
+    """
+    Evaluates a condition and executes the true commands if the condition is true,
+    or the false commands if the condition is false.
+    """
+    if condition:
+        for command in true_commands:
+            execute_command(command)
+    else:
+        for command in false_commands:
+            execute_command(command)
+
+def while_command(condition, commands):
+    """
+    Executes a set of commands repeatedly while a condition is true.
+    """
+    while condition:
+        for command in commands:
+            execute_command(command)
+
+def for_command(variable, iterable, commands):
+    """
+    Executes a set of commands for each element in an iterable.
+    """
+    for element in iterable:
+        execute_command(variable + " = " + repr(element))
+        for command in commands:
+            execute_command(command)
+
+def function_command(name, arguments, commands):
+    """
+    Defines a new function with the specified name, arguments, and commands.
+    """
+    function_definition = "def " + name + "(" + ", ".join(arguments) + "):\n"
+    for command in commands:
+        function_definition += "    " + command + "\n"
+    execute_command(function_definition)
+
+def set_command(variable, value):
+    """
+    Sets the value of a variable.
+    """
+    execute_command(variable + " = " + repr(value))
+
+def read_command(filename):
+    """
+    Reads the contents of a file and returns the contents as a string.
+    """
+    with open(filename, "r") as f:
+        return f.read()
+
+def write_command(filename, contents):
+    """
+    Writes the specified contents to a file.
+    """
+    with open(filename, "w") as f:
+        f.write(contents)
+
+def import_command(module):
+    """
+    Imports a module and makes its functions and variables available in the current scope.
+    """
+    exec("import " + module)
+
+def sleep_command(seconds):
+    """
+    Pauses execution of the script for a specified amount of time.
+    """
+    time.sleep(seconds)
+
+def sqrt_command(x):
+    """
+    Returns the square root of x.
+    """
+    return math.sqrt(x)
+
+def pow_command(x, y):
+    """
+    Returns x raised to the power of y.
+    """
+    return math.pow(x, y)
+
+def sin_command(x):
+    """
+    Returns the sine of x (in radians).
+    """
+    return math.sin(x)
+
+def cos_command(x):
+    """
+    Returns the cosine of x (in radians).
+    """
+    return math.cos(x)
+
+def tan_command(x):
+    """
+    Returns the tangent of x (in radians).
+    """
+    return math.tan(x)
+
+def atan_command(x):
+    """
+    Returns the arctangent of x (in radians).
+    """
+    return math.atan(x)
+
+def command_command(name, commands):
+    """
+    Defines a new command with the specified name and commands.
+    """
+    command_definition = "def " + name + "():\n"
+    for command in commands:
+        command_definition += "    " + command + "\n"
+    execute_command(command_definition)
+
 
 def main():
     """
